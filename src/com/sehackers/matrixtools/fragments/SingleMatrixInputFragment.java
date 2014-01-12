223b34df187589bbe780f9fca20d27e828a27aa7
Clement Hoang <clement.hoang@uwaterloo.ca>
2014-01-12 03:45:39 -0500

    Update # 10: Minor changes

--- src/com/sehackers/matrixtools/fragments/SingleMatrixInputFragment.java
+++ src/com/sehackers/matrixtools/fragments/SingleMatrixInputFragment.java
@@ -2,9 +2,12 @@ package com.sehackers.matrixtools.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 
 import com.sehackers.matrixtools.R;
 
@@ -19,8 +22,9 @@ public class SingleMatrixInputFragment extends Fragment {
             Bundle savedInstanceState) {
  
         View rootView = inflater.inflate(R.layout.fragment_single_matrix_input, container, false);
-        
-        
+        EditText matrixEditText = (EditText) rootView.findViewById(R.id.matrix_text_view);
+        //matrixEditText.setInputType(InputType.TYPE_TEXT_FLAG_MULTI_LINE|InputType.TYPE_CLASS_NUMBER);
+        //matrixEditText.setImeOptions(EditorInfo.IME_ACTION_DONE);
 //        Context context = getActivity();
 //        CharSequence text = "Please enter your input";
 //        int duration = Toast.LENGTH_SHORT;
